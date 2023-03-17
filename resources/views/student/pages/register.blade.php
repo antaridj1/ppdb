b4a508df19e5c549a1958c0785779f707d437dc0
intaniamentari <intan.appkey@gmail.com>
2023-03-17 17:10:07 +0800

    new

--- resources/views/student/pages/register.blade.php
+++ resources/views/student/pages/register.blade.php
@@ -18,7 +18,7 @@
 
   <!-- GOOGLE FONTS -->
   <link href="https://fonts.googleapis.com/css?family=Karla:400,700|Roboto" rel="stylesheet">
-    @include('student.components.cssadmin')
+  @include('student.components.cssadmin')
 </head>
 
 </head>
@@ -26,48 +26,50 @@
           <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh">
           <div class="d-flex flex-column justify-content-between">
             <div class="row justify-content-center">
-              <div class="col-lg-6 col-md-10">
+              <div class="col-lg-6 col-xl-5 col-md-10 ">
                 <div class="card card-default mb-0">
                   <div class="card-header pb-0">
-                    <div class="w-100 d-flex justify-content-center border-bottom-0">
-                        {{-- <img src="images/logo.png" alt="Mono"> --}}
-                        <h3 class="brand-name text-dark font-weight-bold">Login</h3>
+                    <div class="app-brand w-100 d-flex justify-content-center border-bottom-0">
+                      <a class="w-auto pl-0" href="/index.html">
+                        <h3 class="brand-name text-dark font-weight-bold">Buat Akun</h3>
+                      </a>
                     </div>
                   </div>
-                  <div class="card-body px-5 pb-5 pt-0 mt-3">
-
-                    {{-- <h4 class="text-dark mb-6 text-center">Login</h4> --}}
-
+                  <div class="card-body px-5 pb-5 pt-0">
+                    {{-- <h4 class="text-dark text-center mb-5">Sign Up</h4> --}}
                     <form action="/index.html">
                       <div class="row">
                         <div class="form-group col-md-12 mb-4">
-                          <input type="email" class="form-control input-lg" id="email" aria-describedby="emailHelp"
-                            placeholder="email">
+                          <input type="text" class="form-control input-lg" id="name" aria-describedby="nameHelp" placeholder="Name">
+                        </div>
+                        <div class="form-group col-md-12 mb-4">
+                          <input type="email" class="form-control input-lg" id="email" aria-describedby="emailHelp" placeholder="Username">
                         </div>
                         <div class="form-group col-md-12 ">
                           <input type="password" class="form-control input-lg" id="password" placeholder="Password">
                         </div>
+                        <div class="form-group col-md-12 ">
+                          <input type="password" class="form-control input-lg" id="cpassword" placeholder="Confirm Password">
+                        </div>
                         <div class="col-md-12">
+                          <div class="d-flex justify-content-between mb-3">
 
-                          <div class="d-flex justify-content-end mb-5">
-
-                            {{-- <div class="custom-control custom-checkbox mr-3 mb-3">
+                            <div class="custom-control custom-checkbox mr-3 mb-3">
                               <input type="checkbox" class="custom-control-input" id="customCheck2">
-                              <label class="custom-control-label" for="customCheck2">Remember me</label>
-                            </div> --}}
-
-                            <a class="text-color" href="#"> Forgot password? </a>
+                              <label class="custom-control-label" for="customCheck2">I Agree the terms and conditions.</label>
+                            </div>
 
                           </div>
 
-                          <button type="submit" class="btn btn-primary btn-pill mt-3 mb-4 btn-block">Sign In</button>
+                          <button type="submit" class="btn btn-primary btn-pill mb-4">Sign Up</button>
 
-                          <p class="text-center">Belum punya akun ?
-                            <a class="text-blue" href="sign-up.html">Buat Akun</a>
+                          <p class="text-center">Sudah memiliki akun?
+                            <a class="text-blue" href="sign-in.html">Login</a>
                           </p>
                         </div>
                       </div>
                     </form>
+
                   </div>
                 </div>
               </div>
